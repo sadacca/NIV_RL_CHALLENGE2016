9ebb56e24d23b9e8a987ed06882524378b01da79
sadacca <bfsadacca@gmail.com>
2016-08-05 02:09:03 -0400

    nice plot of per-animal alpha/beta

--- rl_plot.py
+++ rl_plot.py
@@ -9,20 +9,30 @@ N = label.size
 x = alpha_beta[:,1]
 y = alpha_beta[:,0]
 
-radii = np.random.random(size=N) * 1.5
+radii = .6 #np.random.random(size=N) * 1.5
 
-colors = [
-    "#%02x%02x%02x" % (int(r), int(g), 150) for r, g in zip(50+2*x, 30+2*y)
-]
 
+palette = ["#053061", "#2166ac", "#4393c3", "#92c5de",
+           "#f7f7f7", "#fddbc7", "#f4a582", "#d6604d", "#b2182b", "#67001f"]
+colors = [palette[np.int16(i)] for i in label[:,0]-1]
 TOOLS="crosshair,pan,wheel_zoom,box_zoom,undo,redo,reset,tap,save,box_select,poly_select,lasso_select"
 
 p = figure(tools=TOOLS)
+
+p = figure(title = "fit values across animals and sessions for states = odorXwell")
 
 p.scatter(x, y, radius=radii,
           fill_color=colors, fill_alpha=0.6,
           line_color=None)
 
-output_file("color_scatter.html", title="color_scatter.py example")
+p.grid.grid_line_color="white"
+p.background_fill_color= "#777777"
+
+
+
+p.xaxis.axis_label = "beta value"
+p.yaxis.axis_label = "alpha value"
+
+output_file("alpha_beta_by_animal_simpleRL.html", title="color_scatter.py example")
 
 show(p)  # open a browser
